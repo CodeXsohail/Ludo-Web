8322c02b605faeb461532db3c1fedbac50613d4d
codeXsohail <sa151598680@gmail.com>
2024-07-26 15:15:31 +0530

    redesigned the layout and movement added for six

--- app.js
+++ app.js
@@ -3,21 +3,75 @@ var diceContainer;
 var diceContainerNext;
 var dicePosition = 1;
 var tmpDiceElement;
+var luckyValue = 0;
+var token = {
+    green: {
+        first: {
+            position: -2
+        },
+        second: {
+            position: -2
+        },
+        third: {
+            position: -2
+        },
+        fourth: {
+            position: -2
+        }
+    },
+    yellow: {
+        first: {
+            position: -2
+        },
+        second: {
+            position: -2
+        },
+        third: {
+            position: -2
+        },
+        fourth: {
+            position: -2
+        }
+    },
+    blue: {
+        first: {
+            position: -2
+        },
+        second: {
+            position: -2
+        },
+        third: {
+            position: -2
+        },
+        fourth: {
+            position: -2
+        }
+    },
+    red: {
+        first: {
+            position: -2
+        },
+        second: {
+            position: -2
+        },
+        third: {
+            position: -2
+        },
+        fourth: {
+            position: -2
+        }
+    }
+};
+
+//default state of pawns which is not released from home
 
 var initGreen = false, initYellow = false, initBlue = false, initRed = false;
 
-var pawnGReleased = [false, false, false, false];
 
-var pawnYReleased = [false, false, false, false];
-
-var pawnBReleased = [false, false, false, false];
-
-var pawnRReleased = [false, false, false, false];
-
-let main = document.querySelector("main");
 
 //layout switcher for mobile/tab/desktop
 
+let main = document.querySelector("main");
 function mobileSize() {
     if (window.innerWidth <= 630) {
         main.classList.remove("main-for-desktop");
@@ -96,26 +150,184 @@ function shiftDice(playerNo) {
 
 document.querySelector('.md1').addEventListener('click', suffleDice);
 
+function savePosition(player,tokenNumber,value,replace = false) {
+    if (replace == true) {
+        if (player == 'g') {
+            if (tokenNumber == "token-g1") {
+                token.green.first.position = value;
+            }
+            else if (tokenNumber == "token-g2") {
+                token.green.second.position = value;
+            }
+            else if (tokenNumber == "token-g3") {
+                token.green.third.position = value;
+            }
+            else if (tokenNumber == "token-g4") {
+                token.green.fourth.position = value;
+            }
+        }
+    
+        if (player == 'y') {
+            if (tokenNumber == "token-y1") {
+                token.yellow.first.position = value;
+            }
+            else if (tokenNumber == "token-y2") {
+                token.yellow.second.position = value;
+            }
+            else if (tokenNumber == "token-y3") {
+                token.yellow.third.position = value;
+            }
+            else if (tokenNumber == "token-y4") {
+                token.yellow.fourth.position = value;
+            }
+        }
+    
+        if (player == 'b') {
+            if (tokenNumber == "token-b1") {
+                token.blue.first.position = value;
+            }
+            else if (tokenNumber == "token-b2") {
+                token.blue.second.position = value;
+            }
+            else if (tokenNumber == "token-b3") {
+                token.blue.third.position = value;
+            }
+            else if (tokenNumber == "token-b4") {
+                token.blue.fourth.position = value;
+            }
+        }
+    
+        if (player == 'r') {
+            if (tokenNumber == "token-r1") {
+                token.red.first.position = value;
+            }
+            else if (tokenNumber == "token-r2") {
+                token.red.second.position = value;
+            }
+            else if (tokenNumber == "token-r3") {
+                token.red.third.position = value;
+            }
+            else if (tokenNumber == "token-r4") {
+                token.red.fourth.position = value;
+            }
+        }
+    } else {
+        if (player == 'g') {
+            if (tokenNumber == "token-g1") {
+                token.green.first.position += value;
+                return token.green.first.position;
+            }
+            else if (tokenNumber == "token-g2") {
+                token.green.second.position += value;
+                return token.green.second.position;
+            }
+            else if (tokenNumber == "token-g3") {
+                token.green.third.position += value;
+                return token.green.third.position
+            }
+            else if (tokenNumber == "token-g4") {
+                token.green.fourth.position += value;
+                return token.green.fourth.position;
+            }
+        }
+    
+        if (player == 'y') {
+            if (tokenNumber == "token-y1") {
+                token.yellow.first.position += value;
+                return token.yellow.first.position;
+            }
+            else if (tokenNumber == "token-y2") {
+                token.yellow.second.position += value;
+                return token.yellow.second.position;
+            }
+            else if (tokenNumber == "token-y3") {
+                token.yellow.third.position += value;
+                return token.yellow.third.position;
+            }
+            else if (tokenNumber == "token-y4") {
+                token.yellow.fourth.position += value;
+                return token.yellow.fourth.position;
+            }
+        }
+    
+        if (player == 'b') {
+            if (tokenNumber == "token-b1") {
+                token.blue.first.position += value;
+                return token.blue.first.position;
+            }
+            else if (tokenNumber == "token-b2") {
+                token.blue.second.position += value;
+                return token.blue.second.position;
+            }
+            else if (tokenNumber == "token-b3") {
+                token.blue.third.position += value;
+                return token.blue.third.position;
+            }
+            else if (tokenNumber == "token-b4") {
+                token.blue.fourth.position += value;
+                return token.blue.fourth.position;
+            }
+        }
+    
+        if (player == 'r') {
+            if (tokenNumber == "token-r1") {
+                token.red.first.position += value;
+                return token.red.first.position;
+            }
+            else if (tokenNumber == "token-r2") {
+                token.red.second.position += value;
+                return token.red.second.position;
+            }
+            else if (tokenNumber == "token-r3") {
+                token.red.third.position += value;
+                return token.red.third.position;
+            }
+            else if (tokenNumber == "token-r4") {
+                token.red.fourth.position += value;
+                return token.red.fourth.position;
+            }
+        }
+    }
+    
 
+}
 
 
 function initPawnBySix(player) {
     let pawns = document.querySelectorAll(`.pawn-${player}`);
-    
+
     function handleClick(event) {
         const clicked = event.target;
+        let onState;
+        let offState;
+        let checkState;
+        let tokenNumber;
+        
         clicked.style.position = "relative";
         clicked.remove();
-        document.querySelector(`.${player}-shell6`).appendChild(clicked);
 
-        let offState = Array.from(clicked.classList).find(cls => cls.startsWith(`pawn-${player}`) && cls.endsWith('-off'));
-        
-        let onState = offState.replace("off", "on");
-        clicked.classList.remove(offState);
-        clicked.classList.add(onState);
+        //when pawn is released
+        checkState = Array.from(clicked.classList).find(cls => cls.startsWith(`pawn-${player}`) && cls.endsWith('-on'));
+        if (checkState) {
+            tokenNumber = Array.from(clicked.classList).find(cls => cls.startsWith(`token-${player}`));
+            let newPosition = savePosition(player,tokenNumber,6);
+            console.log(newPosition);
+            document.querySelector(`.${player}-shell${newPosition}`).appendChild(clicked);
+            console.log(checkState);
+        }
 
-
-        console.log(offState, onState);
+        //when pawn is not released
+        offState = Array.from(clicked.classList).find(cls => cls.startsWith(`pawn-${player}`) && cls.endsWith('-off'));
+        if (offState) {
+            onState = offState.replace("off", "on");
+            clicked.classList.remove(offState);
+            clicked.classList.add(onState);
+            // clicked.classList.add(`p-${player}`);
+            console.log(offState, onState);
+            document.querySelector(`.${player}-shell0`).appendChild(clicked);
+            tokenNumber = Array.from(clicked.classList).find(cls => cls.startsWith(`token-${player}`));
+            savePosition(player,tokenNumber,0,true);
+        }
         pawns.forEach((pawn) => {
             pawn.classList.remove(`pawn-blink-${player}`);
             pawn.removeEventListener("click", handleClick); // Remove event listener
@@ -131,73 +343,97 @@ function initPawnBySix(player) {
 
 
 
-function diceMoveThenShift(luckyValue) {
+function diceMoveThenShift() {
     if (dicePosition == 1) {
         if (initGreen == true) {
             //movements lines
             if (luckyValue == 6) {
                 initPawnBySix('g');
             }
+            else {
+                shiftDice(2);
+            }
 
 
-            shiftDice(2);
+            // shiftDice(2);
 
         }
         else {
             if (luckyValue == 6) {
                 initGreen = true;
                 initPawnBySix('g');
+            } else {
+                shiftDice(2);
             }
 
-            shiftDice(2);
+            // shiftDice(2);
         }
     }
     else if (dicePosition == 2) {
         if (initYellow == true) {
             //movements lines
+            if (luckyValue == 6) {
+                initPawnBySix('y');
+            }
+            else {
+                shiftDice(3);
+            }
 
-
-            shiftDice(3);
+           
         }
         else {
             if (luckyValue == 6) {
                 initYellow = true;
                 initPawnBySix('y');
+            } else {
+                shiftDice(3);
             }
 
-            shiftDice(3);
+           
         }
     }
     else if (dicePosition == 3) {
         if (initBlue == true) {
             //movements lines
+            if (luckyValue == 6) {
+                initPawnBySix('b');
+            } else {
+                shiftDice(4);
+            }
 
-
-            shiftDice(4);
+            
         }
         else {
             if (luckyValue == 6) {
                 initBlue = true;
                 initPawnBySix('b');
+            } else {
+                shiftDice(4);
             }
 
-            shiftDice(4);
+           
         }
     }
     else if (dicePosition == 4) {
         if (initRed == true) {
             //movements lines
 
-
-            shiftDice(1);
+            if (luckyValue == 6) {
+                initPawnBySix('r');
+            } else {
+                shiftDice(1);
+            }
+           
         }
         else {
             if (luckyValue == 6) {
                 initRed = true;
                 initPawnBySix('r');
+            } else {
+                shiftDice(1);
             }
 
-            shiftDice(1);
+          
         }
     }
 }
@@ -215,7 +451,7 @@ function suffleDice() {
         setTimeout(() => {
             tmpDiceElement.style.top = "0px";
         }, 200);
-
+        luckyValue = 1;
         diceMoveThenShift();
     }
     else if (res == 2) {
@@ -227,6 +463,7 @@ function suffleDice() {
         setTimeout(() => {
             tmpDiceElement.style.top = "0px";
         }, 200);
+        luckyValue = 2;
         diceMoveThenShift();
     }
     else if (res == 3) {
@@ -238,6 +475,7 @@ function suffleDice() {
         setTimeout(() => {
             tmpDiceElement.style.top = "0px";
         }, 200);
+        luckyValue = 3;
         diceMoveThenShift();
     }
     else if (res == 4) {
@@ -249,6 +487,7 @@ function suffleDice() {
         setTimeout(() => {
             tmpDiceElement.style.top = "0px";
         }, 200);
+        luckyValue = 4;
         diceMoveThenShift();
     }
     else if (res == 5) {
@@ -260,6 +499,7 @@ function suffleDice() {
         setTimeout(() => {
             tmpDiceElement.style.top = "0px";
         }, 200);
+        luckyValue = 5;
         diceMoveThenShift();
     }
     else {
@@ -271,8 +511,7 @@ function suffleDice() {
         setTimeout(() => {
             tmpDiceElement.style.top = "0px";
         }, 200);
-
-
+        luckyValue = 6;
         diceMoveThenShift(res);
     }
 }
